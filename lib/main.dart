aa3e78cb2e1b4e042aac13243ecf3c398487b258
JD Orcajada <mjdorcajada@tip.edu.ph>
2025-10-21 03:31:50 +0800

    makulitka

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
 import 'home_page.dart'; // your current HomePage file
 import 'package:firebase_auth/firebase_auth.dart';
